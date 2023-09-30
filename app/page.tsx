b221767c74e83e8027b884a6fe9f60f384162092
Shinyn <mattias.alm@hotmail.com>
2023-10-01 01:28:24 +0200

    Updated styling and replaced div with Productcard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import Dropdown from './components/Dropdown';
+import Productcard from './components/ProductCard';
 
 export default function Home() {
   return (
     <>
-      <nav className="p-4 bg-green-200 flex justify-between">
+      <nav className="p-4 w-full bg-green-200 flex justify-between">
         <h1>Hakim Livs</h1>
         <div className="flex space-x-6">
           <Link href="/" className="hover:text-blue-500">
@@ -16,28 +16,34 @@ export default function Home() {
           </Link>
         </div>
       </nav>
-      <section className="hero bg-black text-red-400 h-52 flex items-center justify-center">HERO</section>
-      <main className="flex justify-center">
+      <section className="hero bg-black text-red-400 h-52 w-full flex items-center justify-center">HERO</section>
+      <main className="flex justify-center w-full">
         <aside className="p-4 bg-purple-200 w-52">
           <h2 className="text-center font-bold text-lg">Kategorier</h2>
           <Dropdown />
         </aside>
         <section className="p-4 bg-blue-200">
-          <input type="text" placeholder="Sök artikel" className="border rounded-full p-2" />
-          <div className="productContainer">
-            <div className="produkt">Godis</div>
-            <div className="produkt">Bulle</div>
-            <div className="produkt">Glass</div>
-            <div className="produkt">Pizza</div>
-            <div className="produkt">Köttbullar</div>
-            <div className="produkt">Pannkaka</div>
-            <div className="produkt">Fiskpinnar</div>
-            <div className="produkt">Chips</div>
-            <div className="produkt">Kycklingsallad</div>
+          <div className="flex">
+            <input type="text" placeholder="Sök artikel" className="border rounded-full p-2 m-4" />
+            <button className="hover:text-blue-500">Sök</button>
+          </div>
+          <div className="productContainer grid grid-cols-4 gap-4">
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
+            <Productcard />
           </div>
         </section>
       </main>
-      <footer className="bg-black text-red-400 h-40 flex items-center justify-center absolute bottom-0 w-screen">
+      <footer className="bg-black text-red-400 h-40 flex items-center justify-center w-full">
         <p className="justify-self-center">Footer</p>
       </footer>
     </>
