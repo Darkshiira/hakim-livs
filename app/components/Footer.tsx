150d29ba7b85518f0b89d5b86d90745c46be41ad
Shinyn <mattias.alm@hotmail.com>
2023-10-01 14:10:27 +0200

    Added Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-black text-red-400 h-40 flex items-center justify-center w-full">
+        <p className="justify-self-center">Footer</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
