886b50d1370b516a28a7cddb08061f2fb1dcb5f2
Shinyn <mattias.alm@hotmail.com>
2023-10-01 14:10:39 +0200

    Added Hero component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <>
+      <section className="hero bg-black text-red-400 h-52 w-full flex items-center justify-center">HERO</section>
+    </>
+  );
+};
+
+export default Hero;
