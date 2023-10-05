50c64e826e17f59d64bb7df4d0a3f539073feb56
Shinyn <mattias.alm@hotmail.com>
2023-10-05 14:27:32 +0200

    Adds content to ProductCard dynamically

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,20 +1,31 @@
+'use client';
+
 import Image from 'next/image';
+import { FC } from 'react';
 
 // Mall för produkterna som laddas in på frontpage (kan självklart användas på andra platser där produkter ska in också)
-// TODO: Gör så att innehållet för varje produkt laddas in dynamiskt, lägg till funktion på knapparna, få bilden att ladda in
+// TODO: lägg till funktion på knapparna, få bilden att ladda in
 
-const Productcard = () => {
+// TODO: Ska även ta in en image sen.
+interface ProductCardProps {
+  title: string;
+  manufacturer: string;
+  size: string;
+  price: number;
+}
+
+const Productcard: FC<ProductCardProps> = ({ title, manufacturer, size, price }) => {
   return (
     <>
       <div className="w-52 h-60 bg-red-200 p-2">
         <Image src="/food.webp" alt="food" width={25} height={25} />
-        <h1 className="product-title text-center">Titel</h1>
+        <h1 className="product-title text-center">{title}</h1>
         <div className="flex justify-center space-x-1">
-          <p className="product-manufacturer">Tillverkare</p>
+          <p className="product-manufacturer">{manufacturer}</p>
           <p>|</p>
-          <p className="product-size">Storlek</p>
+          <p className="product-size">{size}</p>
         </div>
-        <h2 className="text-center">399:-</h2>
+        <h2 className="text-center">{price}:-</h2>
         <div className="amountAndPurchase flex bg-green-100 justify-between p-2">
           <div className="flex p-2">
             <button className="decreaseAmount">-</button>
