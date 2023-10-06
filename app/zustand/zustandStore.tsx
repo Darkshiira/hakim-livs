919da81ec461738771f22c3527da04d8579eb9e6
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-06 09:06:34 +0200

    added a category

--- app/zustand/zustandStore.tsx
+++ app/zustand/zustandStore.tsx
@@ -4,15 +4,19 @@ import {create} from 'zustand'
 type State = {
     items:Map<string, string>;
     update: boolean;
+    category: string;
   }
   type Action = {
     updateItems: (items: State['items']) => void
     updateUpdate: (update: State['update']) => void
+    updateCategory: (category: State['category']) => void
   }
   
   export const useItemStore = create<State & Action>((set) => ({
     items: new Map<string, string>(),
     updateItems: (items: Map<string, string>) => set(() => ({ items: items })),
     update: false,
-    updateUpdate: (update: boolean) => set(() => ({ update: update }))
+    updateUpdate: (update: boolean) => set(() => ({ update: update })),
+    category: '',
+    updateCategory: (category: string) => set(() => ({ category: category })),
   }))
