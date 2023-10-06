4a4ebca63f6b41c219db4cbed335ddd0060a7d9a
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-06 09:06:00 +0200

    future filterproducts function in place

--- app/components/ArticleSection.tsx
+++ app/components/ArticleSection.tsx
@@ -1,8 +1,9 @@
-'use client';
+"use client";
 
-import Productcard from './ProductCard';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import Productcard from "./ProductCard";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useItemStore } from "../zustand/zustandStore";
 
 interface ProductData {
   id: string;
@@ -14,24 +15,31 @@ interface ProductData {
 }
 
 const ArticleSection: React.FC = () => {
+  const category = useItemStore((state) => state.category);
   const [products, setProducts] = useState<ProductData[]>([]);
 
   useEffect(() => {
     axios
       .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products`)
       .then(function (response) {
+        //  const filterdproducts = response.data.filter((product: ProductData) => product.category === category);
+        // setProducts(filterdproducts);
         setProducts(response.data);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }, []);
+  }, [category]);
 
   return (
     <>
       <section className="p-4 bg-blue-200">
         <div className="flex justify-center">
-          <input type="text" placeholder="Sök artikel" className="border rounded-full w-80 p-2 m-4" />
+          <input
+            type="text"
+            placeholder="Sök artikel"
+            className="border rounded-full w-80 p-2 m-4"
+          />
           <button className="hover:text-blue-500">Sök</button>
         </div>
         <div className="productContainer grid grid-cols-4 gap-4">
