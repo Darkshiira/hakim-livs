4d92b2a3f4ac7f3ee292862a6e52f171f15ba203
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-10 22:30:37 +0200

    Put a popup on the nav for basket

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,20 +1,25 @@
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import MyPopover from "./Popoverbasket";
 
 // Navbaren högst upp på första sidan
 // TODO: Fixa funktionalitet på login och varukorg
 
 const Nav = () => {
+  const [popUpBasket, setPopUpBasket] = useState(false);
+
   return (
     <>
-      <nav className="p-4 w-full bg-green-200 flex justify-between">
+      <nav className="p-4 w-full bg-green-200 flex justify-between fixed">
         <h1>Hakim Livs</h1>
         <div className="flex space-x-6">
           <Link href="/" className="hover:text-blue-500">
             Logga in
           </Link>
-          <Link href="/" className="hover:text-blue-500">
-            Varukorg
-          </Link>
+          <div>
+            <MyPopover />
+          </div>
         </div>
       </nav>
     </>
