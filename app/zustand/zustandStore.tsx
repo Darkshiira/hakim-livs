53079b895f7f67d56cd9a0409a0574d5e7862dc0
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-10 22:29:37 +0200

    Added a basket zustandstate

--- app/zustand/zustandStore.tsx
+++ app/zustand/zustandStore.tsx
@@ -1,22 +1,26 @@
-import {create} from 'zustand'
+import { create } from "zustand";
 
 // Zustand store to keep track of the items in the category dropdown
 type State = {
-    items:Map<string, string>;
-    update: boolean;
-    category: string;
-  }
-  type Action = {
-    updateItems: (items: State['items']) => void
-    updateUpdate: (update: State['update']) => void
-    updateCategory: (category: State['category']) => void
-  }
-  
-  export const useItemStore = create<State & Action>((set) => ({
-    items: new Map<string, string>(),
-    updateItems: (items: Map<string, string>) => set(() => ({ items: items })),
-    update: false,
-    updateUpdate: (update: boolean) => set(() => ({ update: update })),
-    category: '',
-    updateCategory: (category: string) => set(() => ({ category: category })),
-  }))
+  items: Map<string, string>;
+  update: boolean;
+  category: string;
+  basket: [];
+};
+type Action = {
+  updateItems: (items: State["items"]) => void;
+  updateUpdate: (update: State["update"]) => void;
+  updateCategory: (category: State["category"]) => void;
+  updateBasket: (basket: State["basket"]) => void;
+};
+
+export const useItemStore = create<State & Action>((set) => ({
+  items: new Map<string, string>(),
+  updateItems: (items: Map<string, string>) => set(() => ({ items: items })),
+  update: false,
+  updateUpdate: (update: boolean) => set(() => ({ update: update })),
+  category: "",
+  updateCategory: (category: string) => set(() => ({ category: category })),
+  basket: [],
+  updateBasket: (basket: []) => set(() => ({ basket: basket })),
+}));
