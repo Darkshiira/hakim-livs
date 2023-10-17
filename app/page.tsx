70335b55c08da68af001f88ce7395bfd7c24b2a9
Shinyn <mattias.alm@hotmail.com>
2023-10-17 17:42:40 +0200

    Removed ;

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import MainSection from "../components/MainSection";
-import Nav from "../components/Nav";
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+import MainSection from '../components/MainSection';
+import Nav from '../components/Nav';
 
 export default function Home() {
   return (
     <div className="bg-slate-300">
       <Nav />
       <Hero />
-      <MainSection />;
+      <MainSection />
     </div>
   );
 }
